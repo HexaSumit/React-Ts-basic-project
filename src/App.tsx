ccb57da7876cf6d28af632dc55dccff7d37282be
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-01 23:36:40 +0530

    added App.tsx with products state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import './App.css'
+import  { useState } from 'react'
+import Card from './components/Card'
+import type { IProductType } from "./types/Product"
+const App = () => {
 
-function App() {
-
+  const [products, setProducts] = useState<IProductType[]>([
+    {
+      id: 1,
+      name: 'Phone',
+      price: 3000,
+    }, {
+      id: 2,
+      name: 'Mac',
+      price: 4000,
+    }, {
+      id: 3,
+      name: 'lap',
+      price: 5000,
+    }
+  ])
   return (
     <>
+      hey
+      {products.map((product) => (
+        <Card key={product.id} product={product} />
+      ))}
     </>
   )
 }
