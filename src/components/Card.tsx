0a35f58b19e03899b450244f53e310c18c1e8afd
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-01 23:50:03 +0530

    fixed card to use theme colors

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,9 +7,9 @@ type CardProps = {
 
 const Card = ({product}:CardProps) => {
   return (
-    <div className="bg-[--color-card] text-[--color-card-foreground] rounded-xl shadow-md p-6 hover:shadow-lg transition">
+    <div className="p-6 mb-5 rounded-xl shadow-md bg-card text-card-foreground transition hover:shadow-lg hover:scale-[1.02]">
       <h2 className="text-xl font-semibold">{product.name}</h2>
-      <p className="text-lg text-[--color-foreground]">₹{product.price}</p>
+      <p className="text-lg text-foreground">₹{product.price}</p>
     </div>
   )
 }
